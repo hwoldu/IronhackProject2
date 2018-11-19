ebd55852a7c32277bee6d1914b16f9ade306dcd8
chloehd <chloehipeau@gmail.com>
2018-11-19 16:23:28 +0100

    routes done

---
+++ routes/admin-router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user-model.js");
+
+router.get("/admin/users", (req, res, next) => {
+
+  if (!req.user || req.user.role !== "admin"){
+    req.flash("error", "Only admins can do that. 🏄🏻‍");
+    res.redirect("/");
+    return;
+  }
+  User.find()
+    .sort( { role: 1, createdAt: 1 } )
+    .then(userResults => {
+      res.locals.userArray = userResults;
+      res.render("admin-views/user-list.hbs");
+    })
+    .catch(err => next(err));
+});
+
+
+module.exports = router;
