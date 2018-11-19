d9507f262d8700986c544afb4e9f5e2b12854fdc
hw.petros@gmail.com <hw.petros@gmail.com>
2018-11-19 17:24:24 +0100

    updates

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
-
 const User = require("../models/user-model.js");
-
 const router = express.Router();
+
 
 router.get("/signup", (req, res, next) => {
   res.render("auth-views/signup-form.hbs");
