9c2ce98e75c3e1826615a573abef7d53732fdd0e
chloehd <chloehipeau@gmail.com>
2019-03-05 15:39:20 +0100

    add bootstrap on homepage / broke the black hover

--- bin/recipe-seed.js
+++ bin/recipe-seed.js
@@ -309,7 +309,7 @@ const recipeSchema = [
     ],
     "dishType": "main dish",
     "description": "Heat oven to 220C/fan 200C/gas 7. Tip the potatoes and 1 tbsp of olive oil into an ovenproof dish, then roast the potatoes for 20 mins until starting to brown. Toss the asparagus in with the potatoes, then return to the oven for 15 mins.  Throw in the vinegar and nestle the salmon amongst the vegetables. Drizzle with the remaining oil and return to the oven for a final 10-15 mins until the salmon is cooked. Scatter over the basil leaves and serve everything scooped straight from the dish.",
-    "image": "",
+    "image": "/images/spring/recipes/onepan-salmon-asparagus.jpg",
     "duration": 20,
     "season": "spring"
   },
