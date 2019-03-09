69b3fea7c253a08e6e89331bbd74fa93da716608
chloehd <chloehipeau@gmail.com>
2019-03-09 19:20:22 +0100

    add data

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ var month = date.toString().slice(4, 7);
 
 app.use((req, res, next ) => {
   
-  if ( month === "Mar" ||  month === "Apr" || month === "May"  ) {
+  if ( month === "Apr" || month === "May"  ) {
     res.locals.season = "spring";
 }
   else if  ( month === "Jun" ||  month === "Jul" ||  month === "Aug" ) {
@@ -96,10 +96,10 @@ app.use((req, res, next ) => {
   else if ( month === "Sep" || month === "Oct" || month === "Nov"  ) {
     res.locals.season = "autumn";
   }
-  else if ( month === "Dec" ||  month === "Jan" || month === "Feb" ) {
+  else if ( month === "Dec" ||  month === "Jan" || month === "Feb" || month === "Mar"  ) {
     res.locals.season = "winter";
   }
-  
+
   next();
 });
 
